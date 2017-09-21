d36679f6e9b1d96beade85288a7dd893c29ff910
YanceyR <yancey.reid@gmail.com>
2017-09-20 22:16:12 -0400

    Added random functionality to dice

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let images = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image =
+            UIImage(named: images[Int(arc4random_uniform(6))])
+        diceImageView2.image =
+            UIImage(named: images[Int(arc4random_uniform(6))])
+    }
 }
 
